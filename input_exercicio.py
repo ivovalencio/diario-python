c19ddef1855c6f10acc9de43cb45b2e0fb64e653
ivovalencio <ivovalencio@gmail.com>
2025-05-03 10:15:13 -0300

    Exercícios iniciais em Python

---
+++ input_exercicio.py
@@ -0,0 +1,9 @@
+print(input('Digite o seu salário mensal: '))
+print('Agora vamos tratar de gastos. ')
+print(input('Digite a descrição da despesa: '))
+print(input('Digite o valor da despesa  - ou "sair" para encerrar: '))
+print
+
+
+
+
